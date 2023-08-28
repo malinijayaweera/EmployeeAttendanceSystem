368a5ef932aa57f9b098aa90c5f585192ab5f46d
malinijayaweera <116679562+malinijayaweera@users.noreply.github.com>
2023-08-28 18:36:57 +0530

    backend setup
    initial backend set up

---
+++ backend/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+// const bodyParser = require ('body-parser');
+const cors =require('cors');
+ require('dotenv').config();
+
+// app config
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(cors());
+// app.use(bodyParser.json());
+
+//routes
+
+// connect to db
+const dotenv = require('dotenv');
+dotenv.config({path:'config.env'});
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('mongodb connection successful');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+
+//port
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
